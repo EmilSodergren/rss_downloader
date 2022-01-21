f468d4384940fc6af18bdb395e63e1104d4386b6
Emil Sodergren <EmilSodergren@users.noreply.github.com>
2022-01-21 18:48:41 +0100

    Updated config parsing

--- main.go
+++ main.go
@@ -5,18 +5,43 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type Config struct {
-	DbUrl   string `json:"dburl"`
-	DbName  string `json:"dbname"`
-	RssFile string `json:"rssfile"`
+	DbUrl      string `json:"dburl"`
+	DbName     string `json:"dbname"`
+	SvtRssFile string `json:"rssfile"`
+}
+
+func findConfigFile(path string) (string, error) {
+	if filepath.IsAbs(path) {
+		_, err := os.Stat(path)
+		if err != nil {
+			return "", err
+		}
+		return path, nil
+	}
+	// Look in curdir
+	if _, err := os.Stat(path); err == nil {
+		return path, nil
+	}
+	// Look in /etc
+	newPath := filepath.Join("/", "etc", "rss_downloader", path)
+	if _, err := os.Stat(newPath); err != os.ErrNotExist {
+		return newPath, nil
+	}
+	return "", fmt.Errorf("Could not find %s anywhere", path)
 }
 
 func run() error {
-	confFile, err := os.Open("config.json")
+	configPath, err := findConfigFile("config.json")
+	if err != nil {
+		return err
+	}
+	confFile, err := os.Open(configPath)
 	if err != nil {
 		return fmt.Errorf("Can not open config.json ERROR: %s", err)
 	}
@@ -27,9 +52,13 @@ func run() error {
 		return fmt.Errorf("Can not parse config.json ERROR: %s", err)
 	}
 
-	rssReader, err := os.Open(config.RssFile)
+	svtRssPath, err := findConfigFile(config.SvtRssFile)
 	if err != nil {
-		return fmt.Errorf("Can not open %s ERROR: %s", config.RssFile, err)
+		return err
+	}
+	rssReader, err := os.Open(svtRssPath)
+	if err != nil {
+		return fmt.Errorf("Can not open %s ERROR: %s", config.SvtRssFile, err)
 	}
 
 	// Create the database handle, confirm driver is present
